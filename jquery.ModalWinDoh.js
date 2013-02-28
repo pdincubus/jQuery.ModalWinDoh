22497fe897f5045bbd283db420ef9241cea756e5
Phil Steer <pd@incb.us>
2013-02-28 15:46:16 +0000

    initial commit

---
+++ jquery.ModalWinDoh.js
@@ -0,0 +1,20 @@
+/**
+* ModalWinDoh plugin
+*
+* @author Phil Steer
+* https://github.com/pdincubus/jquery.DarthPhader
+*/
+
+(function($) {
+    $.fn.modalWinDoh = function(options) {
+
+        // settings
+        var settings = $.extend({
+
+        }, options);
+
+        return this.each(function() {
+
+        });
+    };
+})(jQuery);
